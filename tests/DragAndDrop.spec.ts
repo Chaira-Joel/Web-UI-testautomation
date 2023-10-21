a73354ab40e552fd41b216250d3f52688fc9892f
Chaira Joel <chaira.cynthia.joel@gmail.com>
2023-10-21 13:13:25 +0200

    drag and drop test

--- tests/DragAndDrop.spec.ts
+++ tests/DragAndDrop.spec.ts
@@ -1,8 +1,11 @@
 import { test, expect } from "@playwright/test";
 
-test("Drag and drop", async ({ page }) => {
+test("Test drag and drop functionality", async ({ page }) => {
   await page.goto("https://the-internet.herokuapp.com/drag_and_drop");
+  const columnA = page.locator("#column-a");
+  const columnB = page.locator("#column-b");
+  const textBeforeDrag = await columnA.locator("header").textContent(); //get the textcontent of the header before the drag
+  await columnA.dragTo(columnB);
+  const textAfterDrag = await columnA.locator("header").textContent(); // get the textcontent of the header after the drag
+  await expect(textBeforeDrag).not.toEqual(textAfterDrag); //assert that the text content of header A has changed
 });
-
-
-// Div a and Div b 
