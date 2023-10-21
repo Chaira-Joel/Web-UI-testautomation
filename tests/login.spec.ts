b07695fb425b3cf7e31a0bd699b04db20ab1e7f2
Chaira Joel <chaira.cynthia.joel@gmail.com>
2023-10-21 17:16:55 +0200

    login flow update

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,13 +1,19 @@
 import { test, expect } from "@playwright/test";
 
-test.describe("Test the login and logout flow", () => {
+const userName = "tomsmith";
+const passsWord = "SuperSecretPassword!";
+
+const errorName = "tomjohnson";
+const errorPassword = "testnameyes";
+
+test.describe("Test the happy login flow", () => {
   test.beforeEach(async ({ page }) => {
     await page.goto("https://the-internet.herokuapp.com/login");
-    await page.getByLabel("username").fill("tomsmith");
-    await page.getByLabel("password").fill("SuperSecretPassword!");
+    await page.getByLabel("username").fill(userName);
+    await page.getByLabel("password").fill(passsWord);
   });
 
-  test("Check the login flow", async ({ page }) => {
+  test("Test the login flow", async ({ page }) => {
     await page.getByRole("button", { name: "Login" }).click();
     await expect(page.url()).toBe("https://the-internet.herokuapp.com/secure");
   });
@@ -17,6 +23,27 @@ test.describe("Test the login and logout flow", () => {
     const locator = await page.getByRole("link", { name: "Logout" });
     locator.isVisible();
     await locator.click();
+    await expect(page.url()).toBe("https://the-internet.herokuapp.com/login");
+  });
+});
+
+test.describe("Test the unhappy login flow", () => {
+  test("Login is unsuccesfull when invalid username is entered", async ({
+    page,
+  }) => {
+    await page.goto("https://the-internet.herokuapp.com/login");
+    await page.getByLabel("username").fill(errorName);
+    await page.getByLabel("password").fill(passsWord);
+    await page.getByRole("button", { name: "Login" }).click();
+    await expect(page.url()).toBe("https://the-internet.herokuapp.com/login");
+  });
+  test("Login is unsuccesful when invalid password is entered", async ({
+    page,
+  }) => {
+    await page.goto("https://the-internet.herokuapp.com/login");
+    await page.getByLabel("username").fill(userName);
+    await page.getByLabel("password").fill(errorPassword);
+    await page.getByRole("button", { name: "Login" }).click();
     await expect(page.url()).toBe("https://the-internet.herokuapp.com/login");
   });
 });
