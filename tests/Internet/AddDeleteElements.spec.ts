66aad536921d859e7ce98554fcc9ddd03f6d6eda
Chaira Joel <chaira.cynthia.joel@gmail.com>
2023-10-22 15:50:58 +0200

    changes

--- tests/Internet/AddDeleteElements.spec.ts
+++ tests/Internet/AddDeleteElements.spec.ts
@@ -1,18 +1,19 @@
 import { test, expect } from "@playwright/test";
+import { addRemoveSite } from "../../fixtures/data/data";
 
 test("Website has a title", async ({ page }) => {
-  await page.goto("https://the-internet.herokuapp.com/add_remove_elements/");
+  await page.goto(addRemoveSite);
   await expect(page).toHaveTitle(/Internet/);
 });
 
 test("Should add one element", async ({ page }) => {
-  await page.goto("https://the-internet.herokuapp.com/add_remove_elements/");
+  await page.goto(addRemoveSite);
   await page.getByRole("button", { name: "Add Element" }).click();
   await expect(page.getByRole("button", { name: "Delete" })).toBeVisible();
 });
 
 test("Should delete one element", async ({ page }) => {
-  await page.goto("https://the-internet.herokuapp.com/add_remove_elements/");
+  await page.goto(addRemoveSite);
   await page.getByRole("button", { name: "Add Element" }).click();
   const locator = await page.getByRole("button", { name: "Delete" });
   await locator.click();
